ca3cdc22aa6440d1e048e5352f2672e024a289e4
furkle <furkle@furkleindustries.com>
2018-10-12 21:29:49 -0400

    100% coverage for isUUIDTypeIFIDVersion.

---
+++ tests/TypeGuards/isUUIDTypeIFIDVersion.test.ts
@@ -0,0 +1,32 @@
+import {
+  IFIDVersions,
+} from '../../src/Enums/IFIDVersions';
+import {
+  isUUIDTypeIFIDVersion,
+} from '../../src/TypeGuards/isUUIDTypeIFIDVersion';
+
+describe('isUUIDTypeIFIDVersion tests.', () => {
+  it('Returns false if the provided argument is not one of the relevant members of the AGTVersions enum.', () => {
+    expect(isUUIDTypeIFIDVersion('foobarbazbux')).toBe(false);
+  });
+
+  it('Returns true if the provided argument is IFIDVersions.UUID.', () => {
+    expect(isUUIDTypeIFIDVersion(IFIDVersions.UUID)).toBe(true);
+  });
+
+  it('Returns true if the provided argument is IFIDVersions.UUIDv1.', () => {
+    expect(isUUIDTypeIFIDVersion(IFIDVersions.UUIDv1)).toBe(true);
+  });
+
+  it('Returns true if the provided argument is IFIDVersions.UUIDv3.', () => {
+    expect(isUUIDTypeIFIDVersion(IFIDVersions.UUIDv3)).toBe(true);
+  });
+
+  it('Returns true if the provided argument is IFIDVersions.UUIDv4.', () => {
+    expect(isUUIDTypeIFIDVersion(IFIDVersions.UUIDv4)).toBe(true);
+  });
+
+  it('Returns true if the provided argument is IFIDVersions.UUIDv5.', () => {
+    expect(isUUIDTypeIFIDVersion(IFIDVersions.UUIDv5)).toBe(true);
+  });
+});
